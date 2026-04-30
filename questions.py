5786f825705da44bad2ab6fcfddb683810bb13e3
Ankita De <deankita74@gmail.com>
2026-04-30 11:05:23 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,19 @@
+questions = {
+    "easy": [
+        {"question": "What is 2+2?", "options": ["1", "2", "3", "4"], "answer": "4"},
+        {"question": "Color of sky?", "options": ["Blue", "Red", "Green", "Yellow"], "answer": "Blue"},
+        {"question": "Sun rises in?", "options": ["East", "West", "North", "South"], "answer": "East"}
+    ],
+
+    "medium": [
+        {"question": "Capital of India?", "options": ["Mumbai", "Delhi", "Kolkata", "Chennai"], "answer": "Delhi"},
+        {"question": "5 * 6 = ?", "options": ["30", "25", "20", "35"], "answer": "30"},
+        {"question": "Largest ocean?", "options": ["Atlantic", "Indian", "Pacific", "Arctic"], "answer": "Pacific"}
+    ],
+
+    "hard": [
+        {"question": "Time complexity of binary search?", "options": ["O(n)", "O(log n)", "O(n log n)", "O(1)"], "answer": "O(log n)"},
+        {"question": "Binary of 10?", "options": ["1010", "1111", "1001", "1100"], "answer": "1010"},
+        {"question": "Who developed Python?", "options": ["Dennis Ritchie", "Guido van Rossum", "Elon Musk", "James Gosling"], "answer": "Guido van Rossum"}
+    ]
+}
